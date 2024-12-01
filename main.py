be346708993852abd96e723fa82a7585c82d771d
Vadim Burin <burinvad@mail.ru>
2024-12-01 09:48:52 +0300

    add function get_date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card
+from src.widget import get_date
 
 card_number = str(input("введите номер карты: "))
 print(get_mask_card_number(card_number))
@@ -9,3 +10,7 @@ print(get_mask_account(account_number))
 
 user_input = str(input("введите номер карты или счета: "))
 print(mask_account_card(user_input))
+
+user_date = str(input("введите дату: "))
+print(get_date(user_date))
+
--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,11 @@ def mask_account_card(user_input: str) -> str:
         return (user_input[0:-20] + get_mask_account(user_input))
     else:
         return (user_input[0:-16] + get_mask_card_number(user_input))
+
+
+def get_date(user_date: str) -> str:
+    """
+    Функция, которая меняет формат даты
+    """
+    return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}"
+

0ae28c30d5f03175c3105c5bc5ab6946fbbf7943
Vadim Burin <burinvad@mail.ru>
2024-12-06 17:26:23 +0200

    add funcs sort_by_date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+Main.py
 from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 card_number = str(input("введите номер карты: "))
@@ -12,3 +14,19 @@ print(mask_account_card(user_input))
 
 user_date = str(input("введите дату: "))
 print(get_date(user_date))
+
+list_of_dictionaries = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+print(filter_by_state(list_of_dictionaries))
+
+list_operations = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+print(sort_by_date(list_operations))
--- processing.py
+++ processing.py
@@ -10,3 +10,12 @@ def filter_by_state(list_of_dictionaries: list, state: str = "EXECUTED") -> list
             if value == state:
                 new_list_of_dictionaries.append(i)
     return new_list_of_dictionaries
+
+
+def sort_by_date(list_operations: list, reverse: bool = True) -> list:
+    """
+    функция, которая принимает список словарей
+    и возвращает отсортированный список словарей по дате
+    """
+    sorted_list = sorted(list_operations, key=lambda x: x["date"], reverse=reverse)
+    return sorted_list

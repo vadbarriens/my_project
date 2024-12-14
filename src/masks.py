e1c3e32b56e3df03cfa8601871713a334867e283
Vadim Burin <burinvad@mail.ru>
2024-12-14 17:17:10 +0300

    add test_masks, fixes masks.py

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ def get_mask_card_number(card_number: str) -> str:
     """
     Принимает номер карты и возвращает маску
     """
-    return f"{card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+    return f"{card_number[0:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:

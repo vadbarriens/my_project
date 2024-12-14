7b5dcdffa269a49b5774e84d42d4d0c5793fe36f
Vadim Burin <burinvad@mail.ru>
2024-12-14 18:35:09 +0300

    add test_widget, fixes widget

--- src/widget.py
+++ src/widget.py
@@ -6,6 +6,9 @@ def mask_account_card(user_input: str) -> str:
     Функция, которая принимает на вход тип карты или счёта и
     возвращает замаскированный счёт или замаскированный номер карты
     """
+    if user_input == "":
+        raise AssertionError ("Не введены данные")
+
     if user_input[-20:].isnumeric():
         return user_input[0:-20] + get_mask_account(user_input)
     else:
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,3 +1,4 @@
+import pytest
 
 from src.widget import mask_account_card
 
@@ -11,3 +12,19 @@ def test_mask_account_card_Master_card():
 
 def test_mask_account_card_account_number():
     assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
+
+
+@pytest.mark.parametrize("account_card_number, expected",
+                         [("Visa Platinum 7000324564575687", "Visa Platinum 7000 32** **** 5687"),
+                          ("Visa 3849586758493049", "Visa 3849 58** **** 3049"),
+                          ("Maestro 8574637485768594", "Maestro 8574 63** **** 8594"),
+                          ("Счет 73654108430135874305", "Счет **4305")])
+def test_mask_account_card_edge_case(account_card_number, expected):
+    assert mask_account_card(account_card_number) == expected
+
+
+def test_mask_account_card():
+    with pytest.raises(AssertionError):
+        mask_account_card("")
+
+

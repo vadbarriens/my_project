40cef134aa424d3c39af0df29ebf5b76d041f8c8
Vadim Burin <burinvad@mail.ru>
2024-12-14 17:17:28 +0300

    add test_masks, fixes masks.py

---
+++ tests/test_masks.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.masks import get_mask_card_number
+
+def test_get_mask_card_number():
+    assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
+
+
+@pytest.mark.parametrize("card_number, expected", [("3758493047586756473", "3758493 04** **** 6473"),
+                                                   ("5748394857683", "5 74** **** 7683"),
+                                                   ("3748576940", " ** **** 6940")])
+def test_get_mask_card_number_edge_case(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+
+

ab649168d0a04fa8cb13e59e14b04d68e6a84e92
Vadim Burin <burinvad@mail.ru>
2024-12-21 22:22:26 +0300

    add fixture and update README

--- src/generators.py
+++ src/generators.py
@@ -1,21 +1,25 @@
-import random
-
-
-def filter_by_currency(transactions, currency):
+def filter_by_currency(transactions: list, currency: str) -> iter:
+    """Функция, которая принимает на вход список словарей, представляющих транзакции и возвращает итератор,
+    который поочередно выдает транзакции, где валюта операции соответствует заданной"""
 
     return (x for x in transactions if x["operationAmount"]["currency"]["code"] == currency)
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list) -> str:
+    """Генератор, который принимает список словарей с транзакциями и
+    возвращает описание каждой операции по очереди."""
+
     descript_transact = (x.get("description") for x in transactions)
     for x in descript_transact:
         yield x
 
 
-def card_number_generator(start, stop):
-        for x in range(start, stop + 1):
-            necessity_zero = 16 - len(str(x))
-            result = "0" * necessity_zero + str(x)
-            result_number_card = f'{str(result)[:4]} {str(result)[4:8]} {str(result)[-8:-4]} {str(result)[-4:]}'
+def card_number_generator(start: int, stop: int) -> str:
+    """Генератор, который выдает номера банковских карт в заданном диапазоне"""
 
-            yield result_number_card
+    for x in range(start, stop + 1):
+        necessity_zero = 16 - len(str(x))
+        result = "0" * necessity_zero + str(x)
+        result_number_card = f'{str(result)[:4]} {str(result)[4:8]} {str(result)[-8:-4]} {str(result)[-4:]}'
+
+        yield result_number_card

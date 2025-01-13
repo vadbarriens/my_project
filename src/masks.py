93434a675134e9fb2eb804cb5fb2a19c086545c8
Vadim Burin <burinvad@mail.ru>
2025-01-13 21:41:40 +0300

    bugs fixes

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,12 @@
 import logging
 
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filename='../logs/masks_logs.log',
-                    filemode='w', encoding='utf-8')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="../logs/masks_logs.log",
+    filemode="w",
+    encoding="utf-8",
+)
 
 
 logger = logging.getLogger()
@@ -14,9 +16,9 @@ def get_mask_card_number(card_number: str) -> str:
     """
     Принимает номер карты и возвращает маску
     """
-    logger.info('запуск программы')
-    if card_number == None:
-        logger.error('не введен номер карты')
+    logger.info("запуск программы")
+    if card_number == "":
+        logger.error("не введен номер карты")
         raise AssertionError("Не введён номер карты")
 
     return f"{card_number[0:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
@@ -26,5 +28,5 @@ def get_mask_account(account_number: str) -> str:
     """
     Принимает номер счёта и возвращает маску
     """
-    logger.info('маскировка номера счёта')
+    logger.info("маскировка номера счёта")
     return f"**{account_number[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,10 @@
 import json
-from typing import Any
 import logging
+from typing import Any
 
-
-logger = logging.getLogger('utils')
-file_handler = logging.FileHandler('../logs/utils_logs.log', 'w', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("../logs/utils_logs.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
@@ -16,33 +15,33 @@ from src.external_api import convert_transaction
 
 def fin_transaction(path: str) -> list:
     """Возвращает список словарей о финансовых транзакциях"""
-    logger.info('запуск программы')
+    logger.info("запуск программы")
     try:
         with open(path, "r", encoding="utf-8") as transaction_file:
             try:
-                logger.info('загрузка транзакций')
+                logger.info("загрузка транзакций")
                 transaction_list = json.load(transaction_file)
             except json.JSONDecodeError:
-                logger.error('ошибка декодирования файла')
+                logger.error("ошибка декодирования файла")
                 print("Ошибка декодирования файла")
                 return []
     except FileNotFoundError:
-        logger.error('Файл не найден')
+        logger.error("Файл не найден")
         print("Файл не найден")
         return []
     if type(transaction_list) is not list:
-        logger.info('список не содержит транзакций')
+        logger.info("список не содержит транзакций")
         return []
     return transaction_list
 
 
 def transaction_amount(trans: dict) -> Any:
     """Выводит сумму по транзакции в рублях, если валюта иная, то конвертирует её"""
-    logger.info('запуск программы')
+    logger.info("запуск программы")
     if trans["operationAmount"]["currency"]["code"] == "RUB":
-        logger.info('сумма не конвертировалась')
+        logger.info("сумма не конвертировалась")
         amount = float(trans["operationAmount"]["amount"])
     else:
-        logger.info('сумма конвертировалась')
+        logger.info("сумма конвертировалась")
         amount = convert_transaction(trans)
     return amount

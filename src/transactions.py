627fd987b22aebe1c420a5e1729da2dde7cd8247
Vadim Burin <burinvad@mail.ru>
2025-01-17 07:53:18 +0300

    add func read excel

--- src/transactions.py
+++ src/transactions.py
@@ -6,3 +6,9 @@ def read_transactions_csv(path: str) -> list:
     with open(path, "r", encoding="utf-8") as file:
         list_csv_transaction = csv.DictReader(file)
         return list(list_csv_transaction)
+
+
+def read_transactions_excel(path: str) -> list:
+    df = pd.read_excel(path)
+    list_excel_transact = df.to_dict(orient='records')
+    return list(list_excel_transact)

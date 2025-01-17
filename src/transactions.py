38b7e2bffef3fa6221c22170b69ed5d66016b43e
Vadim Burin <burinvad@mail.ru>
2025-01-17 18:45:20 +0300

    add func read csv

---
+++ src/transactions.py
@@ -0,0 +1,7 @@
+import csv
+
+
+def read_transactions_csv(path: str) -> list:
+    with open(path, "r", encoding="utf-8") as file:
+        list_csv_transaction = csv.DictReader(file)
+        return list(list_csv_transaction)

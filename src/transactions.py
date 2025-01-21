62a006e0f76017510ec659d24b64502984efec04
Vadim Burin <burinvad@mail.ru>
2025-01-21 19:36:48 +0300

    add func get_filter_transactions

--- src/transactions.py
+++ src/transactions.py
@@ -1,6 +1,6 @@
 import csv
 import pandas as pd
-
+import re
 
 def read_transactions_csv(path: str) -> list:
     """Читает файл CSV и выдает список словарей транзакций"""
@@ -14,3 +14,13 @@ def read_transactions_excel(path: str) -> list:
     df = pd.read_excel(path)
     list_excel_transact = df.to_dict(orient="records")
     return list(list_excel_transact)
+
+
+def get_filter_transactions(list_dict: list, search_bar: str) -> list:
+    """Фильтрует список транзакций по строке поиска"""
+    list_transactions = []
+    pattern = rf"\b{search_bar}[а-я]*\b"
+    for transact in list_dict:
+        if re.search(pattern, transact.get("description", ""), re.IGNORECASE):
+            list_transactions.append(transact)
+    return list_transactions
